5f778ee837ba645eca7f378b48e29cf8be2c0d1a
nasii05 <iamnasih@gmail.com>
2023-05-11 11:49:34 +0530

    final touchups completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarouselComponent } from './carousel/carousel.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { TrendingComponent } from './layouts/trending/trending.component';
 import { LatestComponent } from './layouts/latest/latest.component';
-import { NowPlayingComponent } from './layouts/now-playing/now-playing.component';
 import { PopularComponent } from './layouts/popular/popular.component';
 import { UpcomingComponent } from './layouts/upcoming/upcoming.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
@@ -20,11 +17,8 @@ import { OwlCarouselComponent } from './layouts/owl-carousel/owl-carousel.compon
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent,
     HomeComponent,
-    TrendingComponent,
     LatestComponent,
-    NowPlayingComponent,
     PopularComponent,
     UpcomingComponent,
     NavbarComponent,
